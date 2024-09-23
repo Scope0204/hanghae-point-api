56f3725abbb44fbebda4caf148d958c0531d4dee
JunGyeongCho <scope0204@gmail.com>
2024-09-23 16:33:37 +0900

    test: 포인트 충전 컨트롤러 테스트 코드 작성

---
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -0,0 +1,50 @@
+package io.hhplus.tdd.point;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.hhplus.tdd.point.service.PointService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = PointController.class)
+class PointControllerTest {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PointService pointService;
+
+    @Test
+    @DisplayName("PATCH 포인트 충전 요청 요청 성공")
+    void chargeSuccessTest() throws Exception{
+        // Given
+        Long id = 1L;
+        Long amount = 50L;
+        UserPoint mockUserPoint = new UserPoint(id, amount, System.currentTimeMillis());
+        when(pointService.charge(anyLong(), anyLong())).thenReturn(mockUserPoint);
+        // 충전 포인트를 json 으로 변환
+        String jsonContent = objectMapper.writeValueAsString(amount);
+
+        // When & Then : patch 요청 수행 후 응답 상태와 반환되는 값 검증
+        mockMvc.perform(patch("/point/{id}/charge", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent))
+                .andExpect(status().isOk()) // response 200
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.point").value(amount));
+    }
+}
